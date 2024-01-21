0bc2250b02014eaf74c7c750f56ad2d20f2a9369
pabsoftware <pabsoftware@outlook.com>
2024-01-21 19:20:00 -0300

    commit inicial

---
+++ dist/index.f75de5e1.js
@@ -0,0 +1,18 @@
+AOS.init();
+const DataDoAniversario = new Date("Jan 24, 2024 19:00:00");
+const timeStampDoAniversario = DataDoAniversario.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaDoAniversario = timeStampDoAniversario - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horasEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteAniversario = Math.floor(distanciaDoAniversario / diaEmMs);
+    const horasAteAniversario = Math.floor(distanciaDoAniversario % diaEmMs / horasEmMs);
+    const minutosAteAniversario = Math.floor(distanciaDoAniversario % horasEmMs / minutoEmMs);
+    const segundoAteAniversario = Math.floor(distanciaDoAniversario % minutoEmMs / 1000);
+    document.getElementById("contador").innerHTML = `Ser\xe1 daqui a ${diasAteAniversario}d ${horasAteAniversario}h ${minutosAteAniversario}m ${segundoAteAniversario}s`;
+}, 1000);
+
+//# sourceMappingURL=index.f75de5e1.js.map
